2d9b8e14d5d1465106cd4492f2a66db29f59633a
achurilov <achurilov@radiofid.ru>
2025-04-14 13:17:00 +0300

    db and migrations

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module achurilov-greenlight
 go 1.24.1
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+	github.com/lib/pq v1.10.9
 )
---
+++ run_gl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o allexport
+source .env
+set +o allexport
+
+# Run Go application using the environment variable
+go run ./cmd/api -port=$PORT -db-dsn=$DNS -env=$ENV -db-max-open-conns=$MAX_CONN -db-max-idle-conns=$MAX_IDLE_CONN -db-max-idle-time=$MAX_IDLE_TIME
