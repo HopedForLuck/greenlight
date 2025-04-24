e5f403f5c469b9de0f794c5047875a1fab2993d6
achurilov <achurilov@radiofid.ru>
2025-04-24 16:29:06 +0300

    gracefull shotdown

--- run_gl.sh
+++ run_gl.sh
@@ -5,4 +5,6 @@ source .env
 set +o allexport
 
 # Run Go application using the environment variable
-go run ./cmd/api -port=$PORT -db-dsn=$DNS -env=$ENV -db-max-open-conns=$MAX_CONN -db-max-idle-conns=$MAX_IDLE_CONN -db-max-idle-time=$MAX_IDLE_TIME
+go run ./cmd/api -port=$PORT -db-dsn=$DNS -env=$ENV \
+    -db-max-open-conns=$MAX_CONN -db-max-idle-conns=$MAX_IDLE_CONN -db-max-idle-time=$MAX_IDLE_TIME \
+    -limiter-rps=$LIMITER_RPS -limiter-burst=$LIMITER_BURST -limiter-enabled=$LIMITER_ENABLED
