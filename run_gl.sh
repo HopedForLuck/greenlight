f0738668ca5eadcf669e36e44fb63dd944889a9c
achurilov <achurilov@radiofid.ru>
2025-05-05 14:41:43 +0300

    email sending through smtp

--- run_gl.sh
+++ run_gl.sh
@@ -5,6 +5,7 @@ source .env
 set +o allexport
 
 # Run Go application using the environment variable
-go run ./cmd/api -port=$PORT -db-dsn=$DNS -env=$ENV \
+go run ./cmd/api -port=$PORT -db-dsn="$DNS" -env=$ENV \
     -db-max-open-conns=$MAX_CONN -db-max-idle-conns=$MAX_IDLE_CONN -db-max-idle-time=$MAX_IDLE_TIME \
-    -limiter-rps=$LIMITER_RPS -limiter-burst=$LIMITER_BURST -limiter-enabled=$LIMITER_ENABLED
+    -limiter-rps=$LIMITER_RPS -limiter-burst=$LIMITER_BURST -limiter-enabled=$LIMITER_ENABLED \
+    -smtp-host="$SMTP_HOST" -smtp-port=$SMTP_PORT -smtp-user=$SMTP_USER -smtp-password=$SMTP_PASSWORD -smtp-sender="$SMTP_SENDER"
